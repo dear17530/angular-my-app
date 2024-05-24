bf93cf1a6d25a05b5f78bbae720d2c5ff7e28ee1
KT <chan.KT@inventec.com>
2024-05-24 11:17:18 +0800

    雙向綁定

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'my-app';
+  item: string = '';
+  list: string[] = ['吃飯', '喝水']
 
   add() {
-    console.log('add called');
+    if (this.item.length > 0) {
+      this.list.push(this.item)
+    }
+    console.log(this.list)
   }
 }
